9a3a651d5ba45e5775e87c087a9df01a2a743253
zg196260 <zg196260@antfin.com>
2020-08-01 10:28:22 +0800

    Test.java

---
+++ src/com/alipay/Test.java
@@ -0,0 +1,13 @@
+package com.alipay;
+
+/**
+ * @Author: 疾风
+ * @Date： 2020-08-01
+ * @Description: com.alipay
+ * @version: 1.0
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
+}
